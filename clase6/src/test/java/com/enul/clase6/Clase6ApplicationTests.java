a65536041466ebdb954005ce39d12bac41291d25
enmanuelneri <enmanuel.uscuvilca@unmsm.edu.pe>
2019-04-16 07:18:54 +0200

    Primer commit

---
+++ clase6/src/test/java/com/enul/clase6/Clase6ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.enul.clase6;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Clase6ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
